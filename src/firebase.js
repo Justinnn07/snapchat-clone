747483f4ab8dae3c283484656367dd1f46d6fe8b
Justinnn07 <justinv2006@gmail.com>
2021-03-07 17:25:52 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfxQSFNJwecfjrc9ScxRMSmHzg_zH8WiM",
+  authDomain: "snapchat-clone-3011.firebaseapp.com",
+  projectId: "snapchat-clone-3011",
+  storageBucket: "snapchat-clone-3011.appspot.com",
+  messagingSenderId: "484960463311",
+  appId: "1:484960463311:web:17ae5bb774aa5d9ebf8aba",
+  measurementId: "G-CNG6V5VW71",
+};
